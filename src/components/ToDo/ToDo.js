837b2f6fd04578f30808f1d08f68ee8629c5077c
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-22 10:48:56 -0500

    ToDo can add a Project

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,10 +1,20 @@
 const {Project} = require('./components/Project/Project');
 const ToDo = (()=>{
     let projects = ((defaultProjectName)=>{
-        let projectList = [Project(defaultProjectName)];
-        return projectList
+        let list = [Project(defaultProjectName)];
+        let names = ()=>{
+            return list.map((project)=>{
+                return project.name;
+            });
+        };
+        return {list, names}
     })('inbox');
-    return {projects}
+    let addProject = (name)=>{
+        let project = Project(name);
+        projects.list.push(project);
+        return project
+    };
+    return {addProject,projects}
 })();
 
 module.exports = {
--- src/components/ToDo/spec/ToDo.test.js
+++ src/components/ToDo/spec/ToDo.test.js
@@ -1,6 +1,11 @@
 const {ToDo} = require('../ToDo');
 describe('ToDo',()=>{
     it('has a default project',()=>{
-        expect(ToDo.projects.length).toBeGreaterThan(0);
+        expect(ToDo.projects.list.length).toBeGreaterThan(0);
+    });
+    it('can add a Project',()=>{
+        let name = 'test';
+        ToDo.addProject(name);
+        expect(ToDo.projects.names()).toContain(name);
     });
 });
