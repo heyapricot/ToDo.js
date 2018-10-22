4572865fccff6d72038e237756b8645029b31254
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-22 09:31:33 -0500

    Task has a priority

--- src/components/Project/components/Task/Task.js
+++ src/components/Project/components/Task/Task.js
@@ -1,12 +1,18 @@
 const {format} = require('date-fns');
-let Task = (description, dueDate)=>{
+const priorityLevels = {
+    "HIGH":0,
+    "NORMAL":1,
+    "LOW":2
+};
+let Task = (description, dueDate, priority)=>{
     let due = ((date)=>{
         let formatted = format(date,'MM/dd/yyyy');
         return {date,formatted}
     })(dueDate);
-    return {description, due}
+    return {description, due, priority}
 };
 
 module.exports = {
+    priorityLevels:priorityLevels,
     Task:Task
 };
--- src/components/Project/components/Task/spec/Task.test.js
+++ src/components/Project/components/Task/spec/Task.test.js
@@ -1,15 +1,19 @@
-const {Task} = require('../Task');
+const {Task, priorityLevels} = require('../Task');
 const {format} = require('date-fns');
 describe('Task',()=>{
     describe('Properties',()=>{
         let description = 'Prepare meeting';
         let dueDate = Date.now();
-        let task = Task(description, dueDate);
+        let priority = priorityLevels.HIGH;
+        let task = Task(description, dueDate, priority);
         it('has a description',()=>{
             expect(task.description).toEqual(description);
         });
         it('has a due date',()=>{
             expect(task.due.formatted).toEqual(format(dueDate,'MM/dd/yyyy'));
+        });
+        it('has a priority level', ()=>{
+            expect(task.priority).toEqual(priority);
         });
     });
 });
