56135c62eac1f0f16ff1500de1f21fddbda54449
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-23 17:30:19 -0500

    Compacted code related to task creator

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -49,60 +49,35 @@ const Home = ((project)=>{
 
     })(node, 'taskTableContainer');
 
+
+
     let taskCreator = ((parentNode)=>{
-        let container = ((parentNode, cssClasses, id)=>{
-            let obj = Bootstrap.createElement('div',cssClasses,parentNode);
-            let node = obj.node;
-            node.id = id;
+        let container = Bootstrap.createElement('div',['container-fluid'],parentNode);
+        let inputCssClasses = ['form-control','form-control-sm', 'bg-transparent', 'border-0', 'text-light'];
+        container.node.id = 'taskCreatorContainer';
+        container.row = Bootstrap.createElement('div',['row','h-100', 'm-1' ,'card', 'bg-transparent', 'border-secondary'],container.node);
+        container.row.col = Bootstrap.createElement('div',['col','d-flex', 'flex-column', 'justify-content-center'],container.row.node);
+        container.row.col.form = Bootstrap.createElement('form',[],container.row.col.node);
+        container.row.col.form.rows = ((parentNode, rowQuantity)=> Array.from(Array(rowQuantity)).map(()=> Bootstrap.createElement('div',['form-row'], parentNode)))(container.row.col.form.node,2);
+        let top = ((row, placeHolderText, inputCss)=>{
+            let col = ((parentNode)=> Bootstrap.createElement('div',['col'],parentNode))(row.node);
+            col.input = ((parentNode)=> Bootstrap.createElement('input',inputCss,parentNode))(col.node);
+            col.input.node.placeholder = placeHolderText;
+            return {col}
+        })(container.row.col.form.rows[0], 'Task Description', inputCssClasses);
 
-            let row = ((parentNode, cssClasses)=>{
-                let obj = Bootstrap.createElement('div',['row', ...cssClasses],parentNode);
-                let node = obj.node;
-                let col = ((parentNode, cssClasses)=>{
-                    let obj = Bootstrap.createElement('div',['col',...cssClasses],parentNode);
-                    let node = obj.node;
-                    let card = ((parentNode, cssClasses)=>{
-                        let obj = Bootstrap.createElement('div', ['card',...cssClasses], parentNode);
-                        let node = obj.node;
-                        let body = ((parentNode)=>{
-                            let obj = Bootstrap.createElement('div',['card-body'],parentNode);
-                            let node = obj.node;
-                            let form = ((parentNode)=>{
-                                let obj = Bootstrap.createElement('form',[],parentNode);
-                                let node = obj.node;
-                                let rows = ((parentNode, rowQuantity)=> Array.from(Array(rowQuantity)).map(()=> Bootstrap.createElement('div',['form-row'], parentNode)))(node,2);
-
-                                let inputCssClasses = ['form-control','form-control-sm', 'bg-transparent', 'border-0', 'text-light'];
-
-                                let top = ((row, placeHolderText, inputCss)=>{
-                                    let col = ((parentNode)=> Bootstrap.createElement('div',['col'],parentNode))(row.node);
-                                    col.input = ((parentNode)=> Bootstrap.createElement('input',inputCss,parentNode))(col.node);
-                                    col.input.node.placeholder = placeHolderText;
-                                    return {col}
-                                })(rows[0], 'Task Description', inputCssClasses);
-
-                                let bottom = ((row, inputCss)=>{
-                                    let columns = ((parentNode, columnQuantity)=> Array.from(Array(columnQuantity)).map(()=>Bootstrap.createElement('div',['col-auto'],parentNode)))(row.node,3);
-                                    columns[0].dateInput = ((parentNode)=> Bootstrap.createElement('input',inputCss,parentNode))(columns[0].node);
-                                    columns[0].dateInput.node.type = 'Date';
-                                    columns[1].buttonGroup = ((parentNode)=> Bootstrap.createElement('div', ['btn-group', 'btn-group-sm'],parentNode))(columns[1].node);
-                                    columns[1].buttonGroup.buttons = ((parentNode, quantity, buttonClasses)=> Array.from(Array(quantity)).map((elem,index)=>{
-                                        let obj = Bootstrap.createElement('div',['btn', ...buttonClasses[index]],parentNode);
-                                        obj.node.textContent = index;
-                                    }))(columns[1].buttonGroup.node,3,[['btn-outline-primary'], ['btn-outline-success'], ['btn-outline-danger']]);
-                                    columns[2].newTaskButton = ((parentNode)=> Bootstrap.createElement('div',['btn','btn-success', 'btn-sm'],parentNode))(columns[2].node);
-                                    columns[2].newTaskButton.node.textContent = 'New Task';
-                                })(rows[1], inputCssClasses);
-
-                            })(node);
-                        })(node);
-                        return {node}
-                    })(node, ['border-secondary', 'bg-transparent']);
-                })(node, ['d-flex', 'flex-column', 'justify-content-center']);
-            })(node, ['h-100']);
-
-            return {node,row}
-        })(parentNode, ['container-fluid'], 'taskCreatorContainer');
+        let bottom = ((row, inputCss)=>{
+            let columns = ((parentNode, columnQuantity)=> Array.from(Array(columnQuantity)).map(()=>Bootstrap.createElement('div',['col-auto'],parentNode)))(row.node,3);
+            columns[0].dateInput = ((parentNode)=> Bootstrap.createElement('input',inputCss,parentNode))(columns[0].node);
+            columns[0].dateInput.node.type = 'Date';
+            columns[1].buttonGroup = ((parentNode)=> Bootstrap.createElement('div', ['btn-group', 'btn-group-sm'],parentNode))(columns[1].node);
+            columns[1].buttonGroup.buttons = ((parentNode, quantity, buttonClasses)=> Array.from(Array(quantity)).map((elem,index)=>{
+                let obj = Bootstrap.createElement('div',['btn', ...buttonClasses[index]],parentNode);
+                obj.node.textContent = index;
+            }))(columns[1].buttonGroup.node,3,[['btn-outline-primary'], ['btn-outline-success'], ['btn-outline-danger']]);
+            columns[2].newTaskButton = ((parentNode)=> Bootstrap.createElement('div',['btn','btn-success', 'btn-sm'],parentNode))(columns[2].node);
+            columns[2].newTaskButton.node.textContent = 'New Task';
+        })(container.row.col.form.rows[1], inputCssClasses);
     })(node);
 
 
