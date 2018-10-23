0adda24e9c7f60988277f0e9eee0abb8ed51a253
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-23 18:16:56 -0500

    Removed border from priority buttons and added icon

--- src/scenes/Home/components/TaskCreator/TaskCreator.js
+++ src/scenes/Home/components/TaskCreator/TaskCreator.js
@@ -13,13 +13,22 @@ let TaskCreator = (()=>{
 
     let bottom = ((row, inputCss)=>{
         let columns = ((parentNode, columnQuantity)=> Array.from(Array(columnQuantity)).map(()=>Bootstrap.createElement('div',['col-auto'],parentNode)))(row.node,3);
-        columns[0].dateInput = ((parentNode)=> Bootstrap.createElement('input',inputCss,parentNode))(columns[0].node);
+        columns[0].dateInput = Bootstrap.createElement('input',inputCss,columns[0].node);
         columns[0].dateInput.node.type = 'Date';
-        columns[1].buttonGroup = ((parentNode)=> Bootstrap.createElement('div', ['btn-group', 'btn-group-sm'],parentNode))(columns[1].node);
-        columns[1].buttonGroup.buttons = ((parentNode, quantity, buttonClasses)=> Array.from(Array(quantity)).map((elem,index)=>{
-            let obj = Bootstrap.createElement('div',['btn', ...buttonClasses[index]],parentNode);
-            obj.node.textContent = index;
-        }))(columns[1].buttonGroup.node,3,[['btn-outline-primary'], ['btn-outline-success'], ['btn-outline-danger']]);
+        columns[1].buttonGroup = Bootstrap.createElement('div', ['btn-group', 'btn-group-sm'],columns[1].node);
+
+        let buttonQuantity = 3;
+        let iconClasses = Array.from(Array(buttonQuantity)).map(()=>['fas','fa-flag']);
+        console.log(iconClasses);
+
+        let buttonClasses = ['btn-outline-primary','btn-outline-success','btn-outline-danger'].map((elem)=>[elem,'border-0']);
+
+        columns[1].buttonGroup.buttons = ((parentNode, quantity, cssClasses, iconClasses )=> Array.from(Array(quantity)).map(
+            (elem,index)=>{
+                let obj = Bootstrap.createElement('div',['btn', ...cssClasses[index]],parentNode);
+                obj.node.icon = Bootstrap.createElement('i',iconClasses[index],obj.node);
+            }))(columns[1].buttonGroup.node,3,buttonClasses, iconClasses);
+
         columns[2].newTaskButton = ((parentNode)=> Bootstrap.createElement('div',['btn','btn-success', 'btn-sm'],parentNode))(columns[2].node);
         columns[2].newTaskButton.node.textContent = 'New Task';
     })(form.rows[1], inputCssClasses);
