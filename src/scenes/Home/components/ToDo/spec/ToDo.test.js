0e34cd7314de5853c89d0c23206f44cb54c1f457
Alvaro Sanchez <heyalvaro@gmail.com>
2018-10-30 11:57:54 -0600

    Fixed ToDo spec

--- src/scenes/Home/components/ToDo/spec/ToDo.test.js
+++ src/scenes/Home/components/ToDo/spec/ToDo.test.js
@@ -1,16 +1,16 @@
 const {ToDo} = require('../ToDo');
 describe('ToDo',()=>{
     it('has a default project',()=>{
-        expect(ToDo.projects.list.length).toBeGreaterThan(0);
+        expect(ToDo.projects.length).toBeGreaterThan(0);
     });
     it('can add a Project',()=>{
         let name = 'test';
         ToDo.addProject(name);
-        expect(ToDo.projects.names()).toContain(name);
+        expect(ToDo.projectNames()).toContain(name);
     });
     it('can add a Task',()=>{
         let description = 'test';
         ToDo.addTask(description);
-        expect(ToDo.currentProject.tasks.descriptions()).toContain(description);
+        expect(ToDo.getActiveProject().taskDescriptions()).toContain(description);
     });
 });
